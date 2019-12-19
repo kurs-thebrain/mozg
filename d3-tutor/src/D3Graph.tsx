3b6b03fd4e1b09a2391dd6e2e8f4b5f8fcafa5bd
h4rdc0r31 <romchikarmani@gmail.com>
2019-12-19 19:09:13 +0400

    changes

--- d3-tutor/src/D3Graph.tsx
+++ d3-tutor/src/D3Graph.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {mouse, selectAll} from 'd3-selection'
-import {forceCenter, forceLink, forceManyBody, forceSimulation} from 'd3-force'
+import {forceCenter, forceLink, forceManyBody, forceSimulation, forceLayout} from 'd3-force'
 import data from './data.json'
 import {scaleOrdinal} from "d3-scale";
 import {schemeCategory10} from "d3-scale-chromatic"
@@ -36,11 +36,11 @@ export default class D3Graph extends Component<any,any> {
     svg = selectAll;
     container = selectAll;
     node = selectAll;
+    nodes = [{}];
     link = selectAll;
     drag_line = selectAll;
     colorScale = scaleOrdinal(schemeCategory10)
 
-    // пихни это куда-нибудь туда ниже
     mousedown = () => {
         this.setState({dragged: true})
         if (this.state.mousedownNode && this.state.dragged)
@@ -60,8 +60,49 @@ export default class D3Graph extends Component<any,any> {
     }
 
     mouseup = () => {
+        if (this.state.mouseup_node == this.state.mousedownNode)
+        {
+            if (this.state.dragged) {
+                this.setState({dragged: false, mousedownNode: null, mouseup_node: null})
+                this.drag_line
+                    .attr("class", "hidden_line")
+                    .attr("x1", 0)
+                    .attr("y1", 0)
+                    .attr("x2", 0)
+                    .attr("y2", 0)
+            }
+            return;
+        }
+
+        if (this.state.mouseup_node && this.state.mousedownNode)
+        {
+            let newLink = {source: this.state.mousedownNode, target: this.state.mouseup_node};
+            this.container
+                .selectAll('.links')
+                .append('line')
+                .attr('class', 'link')
+
+            this.state.graph.links.push(newLink)
+            this.updateGraph()
+        }
+        else if (this.state.mousedownNode)
+        {
+            this.addNode('kekw', mouse(this.svg.node())[0], mouse(this.svg.node())[1])
+
+            let newLink = {source: this.state.mousedownNode, target: this.state.graph.nodes.length}
+
+            this.container
+                .selectAll('.links')
+                .append('line')
+                .attr('class', 'link')
+
+            this.state.graph.links.push(newLink)
+
+            this.updateGraph();
+        }
+
         if (this.state.dragged) {
-            this.setState({dragged: false, mousedownNode: null})
+            this.setState({dragged: false, mousedownNode: null, mouseup_node: null})
             this.drag_line
                 .attr("class", "hidden_line")
                 .attr("x1", 0)
@@ -108,6 +149,10 @@ export default class D3Graph extends Component<any,any> {
             .on('mousedown', (d:any) => {
                 this.setState({currentNode: d, mousedownNode: d})
             })
+            .on('mouseup', (d:any)=>
+            {
+                this.setState({currentNode: d, mouseup_node: d})
+            })
             .on('click', (d:any) => {
                 this.setState({currentNode: d, isVisible: true})
             })
@@ -140,49 +185,44 @@ export default class D3Graph extends Component<any,any> {
     }
 
     updateGraph() {
-       // const colorScale = scaleOrdinal(schemeCategory10) // работает не так, как нужно
+        this.node = this.container.selectAll('.nodes').selectAll('.node').data(this.state.graph.nodes)
+        this.link = this.container.selectAll('.links').selectAll('.link').data(this.state.graph.links)
 
-        // this.link = this.link.data(this.state.graph.links)
-
-        // this.link.enter()
-        //     .insert("line", ".node")
-        //     .attr("class", "link") // узнать
-        //
-        // this.link.exit()
-        //     .remove();
-        // this.link
-        //     .classed("link_selected", (d:any) => {return d === this.state.selected_link});
-
-        //console.log(this.node.data(this.state.graph.nodes))
-
-        this.node = selectAll('circle')
-            .data(this.state.graph.nodes)
-            .enter()
-                .append('circle')
-                .attr('class', 'node')
-                .attr('r', 15)
-            .exit()
-                .append('circle')
-                .attr('class', 'node')
-                .attr('r', 15)
-                .attr('fill','green')
-        // this.node
-        //     .classed("node_selected", (d:any) => {return d === this.state.selected_node})
-
-        console.log(this.node)
+        this.graph.nodes(this.state.graph.nodes)
+        this.graph.force('link').links(this.state.graph.links)
+        this.graph.force('charge', forceManyBody().strength(-200))
+            .force('center', forceCenter(this.state.width/2, this.state.height/2))
+        this.graph.alpha(0.1).restart()
     }
 
-    addNode = (content:string) => {
+    addNode = (content:string, x:number, y:number) => {
         let newGraph = {
             'nodes': [...this.state.graph.nodes,
-                {   id: this.state.graph.length,
+                {   id: this.state.graph.nodes.length + 1,
                     contentType: 'markdown',
                     content: content
                 }],
             'links': this.state.graph.links
         }
-        this.setState({graph: newGraph})
-        this.updateGraph();
+
+        this.container
+            .selectAll('.nodes')
+            .append('circle')
+            .attr('class', 'node')
+            .attr('r', 15)
+            .attr('fill', (d:any) => this.colorScale(d))
+            .on('mousedown', (d:any) => {
+                this.setState({currentNode: d, mousedownNode: d})
+            })
+            .on('mouseup', (d:any)=>
+            {
+                this.setState({currentNode: d, mouseup_node: d})
+            })
+            .on('click', (d:any) => {
+                this.setState({currentNode: d, isVisible: true})
+            })
+
+        this.setState({graph: newGraph}, this.updateGraph)
     }
 
     handleWindowSize = () => {
@@ -195,7 +235,6 @@ export default class D3Graph extends Component<any,any> {
     componentDidMount() {
         window.addEventListener('resize',this.handleWindowSize)
         this.initializeGraph()
-       // this.updateGraph()
     }
 
     componentWillUnmount(){
@@ -208,120 +247,15 @@ export default class D3Graph extends Component<any,any> {
 
         return (
             <div>
-                <Dock position='left' isVisible={this.state.isVisible} dimMode='opaque'>
+                <Dock position='left' isVisible={this.state.isVisible} dimMode='transparent'>
                     <button onClick={() => {this.setState({isVisible: false})}}>Exit</button>
                     {toDraw}
                 </Dock>
                 <svg width={this.state.width} height={this.state.height} style={{border:'solid 1px #eee', borderBottom:'solid 1ox #ccc'}}>
                     <g className='graph'/>
                 </svg>
-                <button onClick={() => {this.addNode('new')}}>New node</button>
+                <button onClick={() => {this.addNode('new', mouse(this)[0], mouse(this)[1])}}>New node</button>
             </div> )
 
     }
 }
-
-
-
-// const mousedown = () => {
-//     this.setState({dragged: true})
-//     if (this.state.mousedownNode && this.state.dragged)
-//         drag_line
-//             .attr("class", "drag_line")
-//             .attr("x1", this.state.currentNode.x)
-//             .attr("y1", this.state.currentNode.y)
-//             .attr("x2", this.state.currentNode.x)
-//             .attr("y2", this.state.currentNode.y);
-// }
-//
-// const mousemove = () => {
-//     if (this.state.dragged)
-//         drag_line
-//             .attr("x2", mouse(svg.node())[0])
-//             .attr("y2", mouse(svg.node())[1]);
-// }
-//
-// const mouseup = () => {
-//     if (this.state.dragged) {
-//         this.setState({dragged: false, mousedownNode: null})
-//         drag_line
-//             .attr("class", "hidden_line")
-//             .attr("x1", 0)
-//             .attr("y1", 0)
-//             .attr("x2", 0)
-//             .attr("y2", 0)
-//     }
-// }
-
-// const svg = selectAll('svg')
-//     .on("mousemove", mousemove)
-//     .on("mousedown", mousedown)
-//     .on("mouseup", mouseup)
-
-// const container = selectAll('g')
-//
-// const drag_line = container.append("line") // линия, которая отображается при создании новых узлов
-//     .attr("class", "hidden_line") // добавить в css кастомный класс
-//     .attr("x1", 0)
-//     .attr("y1", 0)
-//     .attr("x2", 0)
-//     .attr("y2", 0)
-//
-//
-//
-// const link = container
-//     .append('g')
-//     .attr('class', 'links') // связи
-//     .selectAll('line')
-//     .data(this.state.graph.links)
-//     .enter()
-//     .append('line')
-//     .attr('class', 'link')
-//
-//
-// const node = container
-//     .append('g')
-//     .attr('class', 'nodes') // узлы
-//     .selectAll('circle')
-//     .data(this.state.graph.nodes)
-//     .enter()
-//     .append('circle')
-//     .attr('class', 'node')
-//     .attr('r', 15)
-//     .attr('fill', (d:any) => colorScale(d))
-//     .on('mousedown', (d:any) => {
-//         this.setState({currentNode: d, mousedownNode: d})
-//     })
-//     .on('click', (d:any) => {
-//         this.setState({currentNode: d, isVisible: true})
-//     })
-//
-// const labels = node
-//     .append('text')
-//     .text((d:any) => d.label)
-//     .attr('x', (d:any) => d.x)
-//     .attr('y', (d:any) => d.y)
-//
-// node.append('title')
-//     .text((d:any) => d.label)
-//
-// function ticked() {
-//     node.call(updateNode)
-//     link.call(updateLink)
-// }
-//
-// function updateNode(node:any) {
-//     node.attr('transform', (d:any) => `translate(${fixna(d.x)},${fixna(d.y)})`)
-// }
-//
-// function updateLink(link:any) {
-//     link.attr('x1', (d:any) => fixna(d.source.x))
-//     link.attr('y1', (d:any) => fixna(d.source.y))
-//     link.attr('x2', (d:any) => fixna(d.target.x))
-//     link.attr('y2', (d:any) => fixna(d.target.y))
-// }
-//
-// function fixna(x:any) {
-//     if (isFinite(x)) return x
-//     return 0
-// }
